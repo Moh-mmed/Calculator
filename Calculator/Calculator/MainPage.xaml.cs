e7edeac423a995b0e23f2e91c29a1811eeaad476
mohammedBnomr <moohaboy@gmail.com>
2021-11-19 18:35:23 +0300

    Add project files.

---
+++ Calculator/Calculator/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        double opr1, opr2, disp_val;
+        string disp_string;
+        int operation_flag, floating_point_flag, ongoig_flag;
+
+        public MainPage()
+        {
+        }
+
+        // Buttons 
+        private void Button_C_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Sign_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Div_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Button_7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_9_Clicked(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void Button_Mul_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void Button_4_Clicked(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void Button_5_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void Button_6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Sub_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void Button_1_Clicked(object sender, EventArgs e)
+        {
+       
+        }
+        private void Button_2_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_3_Clicked(object sender, EventArgs e)
+        {
+ 
+        }
+        private void Button_Add_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+
+
+        private void Button_Zero_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+        private void Button_Dot_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void Button_Equal_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
